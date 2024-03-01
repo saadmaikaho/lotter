ebf309f4dcf0e0b8bb1223461f2bfcf11d864af6
saadmaikaho <saadghazalisalisu@gmail.com>
2024-03-01 11:45:13 +0800

    commit

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://tkcompany.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],

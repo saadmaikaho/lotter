f9e217a60f15b5a5e486ee1c7ad7f89717a6c28b
saadmaikaho <saadghazalisalisu@gmail.com>
2024-03-01 14:37:15 +0800

    commit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Add CORS middleware to allow requests from your frontend domain
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://tradegiftcard.net"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
